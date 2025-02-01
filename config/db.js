4808d894590f29b6ed30d6116b9844b8c62116f5
John Doe <sanjeev.kumar@ksolves.com>
2025-02-01 11:06:52 +0530

    Database connection stablished

--- config/db.js
+++ config/db.js
@@ -1,7 +1,11 @@
 const mongoose = require('mongoose');
+// aDjptO4XV1unf5IB
+// sanjeev718191
+//mongodb+srv://sanjeev718191:aDjptO4XV1unf5IB@test-loop-mail.qzsg3.mongodb.net/?retryWrites=true&w=majority&appName=test-loop-mail
 const connectDB = async () => {
     
     const conn = await mongoose.connect(process.env.MONGO_URI);
+    // const conn = await mongoose.connect('mongodb+srv://sanjeev718191:aDjptO4XV1unf5IB@test-loop-mail.qzsg3.mongodb.net/?retryWrites=true&w=majority&appName=test-loop-mail');
     console.log(`MongoDB connected : ${conn.connection.host}`.cyan.bold);
 }
 module.exports = connectDB;
